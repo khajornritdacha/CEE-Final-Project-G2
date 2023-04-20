af12307999ad8d198f4c7bc5aefbbdf5d744ff9e
vijak khajornritdacha <vijak13019@hotmail.com>
2023-04-20 17:57:11 +0700

    feat: add assigned assignments API

--- src/controller/assignmentsController.js
+++ src/controller/assignmentsController.js
@@ -5,24 +5,30 @@ const {
   BatchWriteItemCommand,
 } = require('@aws-sdk/client-dynamodb');
 const { PutCommand, ScanCommand } = require('@aws-sdk/lib-dynamodb');
+const { default: axios } = require('axios');
 
 const docClient = new DynamoDBClient({ regions: process.env.AWS_REGION });
 
-// 1. get assignments from db
-// 2. get assignments from mcv
-// 3. compare, if not in db, add to db
-// 4. return assignments from db
-
 const writeAssignments = async (updateItemRequest) => {
-  const params = {
-    RequestItems: {
-      [process.env.aws_assignments_table_name]: updateItemRequest,
-    },
-  };
+  if (updateItemRequest <= 0) return;
 
   try {
-    const data = await docClient.send(new BatchWriteItemCommand(params));
-    console.log('Success, items inserted', data);
+    let promises = [];
+    let updateReq = [];
+    for (let i = 0; i < updateItemRequest.length; i++) {
+      updateReq.push(updateItemRequest[i]);
+      // BatchWriteItem can only write 25 items at a time
+      if (updateReq.length === 25 || i === updateItemRequest.length - 1) {
+        const params = {
+          RequestItems: {
+            [process.env.aws_assignments_table_name]: updateReq,
+          },
+        };
+        promises.push(docClient.send(new BatchWriteItemCommand(params)));
+        updateReq = [];
+      }
+    }
+    const data = await Promise.all(promises);
     return data;
   } catch (err) {
     console.log('Error', err);
@@ -30,30 +36,87 @@ const writeAssignments = async (updateItemRequest) => {
   }
 };
 
-exports.readAssignmentsMcv = async (req, _res) => {
+const getCourses = async (access_token, options) => {
+  const { year, semester, course_no } = options;
   try {
-    const data = await fetch(
-      'https://www.mycourseville.com/api/v1/public/get/user/courses',
-      {
-        headers: {
-          Authorization: `Bearer ${'SugUwd5Npz44aYZs95TtsIimMyb2dEBr5nELRKmZ'}`,
-        },
-      }
+    const url = new URL(
+      'https://www.mycourseville.com/api/v1/public/get/user/courses?detail=1'
     );
-    console.log(data);
+    const data = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+    const courses = data.data.data.student;
+    if (!year || !semester)
+      throw new Error('Year or semester is not provided.');
+    const filteredCourses = courses.filter(
+      (item) =>
+        String(item.year) === String(year) &&
+        String(item.semester) === String(semester) &&
+        (!course_no || item.course_no === course_no)
+    );
+    return filteredCourses;
   } catch (err) {
     console.log(err);
+    return null;
   }
-  _res.sendStatus(200);
-  //   return [];
 };
 
-const readAssignmentsDb = async () => {
+const getCoursesAssignments = async (access_token, courses) => {
+  const promises = [];
+  try {
+    for (const course of courses) {
+      const url = new URL(
+        'https://www.mycourseville.com/api/v1/public/get/course/assignments'
+      );
+      url.searchParams.append('cv_cid', course.cv_cid);
+      url.searchParams.append('detail', '1');
+      promises.push(
+        axios.get(url, {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+          transformResponse: [
+            (data) => {
+              const assignments = JSON.parse(data).data;
+              return assignments.map((item) => ({
+                ...item,
+                course,
+              }));
+            },
+          ],
+        })
+      );
+    }
+    const data = await Promise.all(promises);
+    return data.reduce((acc, item) => {
+      return acc.concat(item.data);
+    }, []);
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+const readAssignmentsDb = async (options) => {
   const params = {
     TableName: process.env.aws_assignments_table_name,
   };
   try {
-    const { Items } = await docClient.send(new ScanCommand(params));
+    let { Items } = await docClient.send(new ScanCommand(params));
+    if (!!options && options?.year && options?.semester) {
+      Items = Items.filter(
+        (item) =>
+          item.course.year == options.year &&
+          item.course.semester == options.semester
+      );
+    }
+    if (!!options && !!options?.course_no) {
+      Items = Items.filter(
+        (item) => item.course.course_no === options.course_no
+      );
+    }
     return Items;
   } catch (err) {
     console.error(err);
@@ -61,31 +124,86 @@ const readAssignmentsDb = async () => {
   }
 };
 
-const syncAssignments = async () => {
-  const dbItems = readAssignmentsDb();
-  const mcvItems = [];
-  const updateItemRequest = [];
-  for (const item of dbItems) {
-    if (!mcvItems.includes(item.item_id)) {
-      updateItemRequest.push({
-        PutRequest: {
-          Item: {
-            item_id: { S: item.item_id },
-            student_id: { S: '6532155621' },
-            isFinished: { BOOL: false },
+const syncAllCoursesAssignments = async (access_token, options) => {
+  try {
+    const dbItems = await readAssignmentsDb(options);
+    const dbItemsId = dbItems.map((item) => item.item_id);
+    const courses = await getCourses(access_token, options);
+    const mcvItems = await getCoursesAssignments(access_token, courses);
+    const updateItemRequest = [];
+    for (const item of mcvItems) {
+      if (!dbItemsId.includes(item.itemid)) {
+        updateItemRequest.push({
+          PutRequest: {
+            Item: {
+              item_id: { S: String(item.itemid) },
+              student_id: { S: '6532155621' },
+              is_finished: { BOOL: false },
+              title: { S: item.title },
+              out_time: { S: String(item.changed) },
+              due_time: { S: String(item.duetime) },
+              course: {
+                M: {
+                  cv_cid: { S: String(item.course.cv_cid) },
+                  course_no: { S: String(item.course.course_no) },
+                  title: { S: item.course.title },
+                  year: { S: String(item.course.year) },
+                  semester: { S: String(item.course.semester) },
+                  course_icon: { S: item.course.course_icon },
+                },
+              },
+            },
           },
-        },
-      });
+        });
+      }
     }
+    await writeAssignments(updateItemRequest);
+  } catch (err) {
+    console.log(err);
   }
-  await writeAssignments(updateItemRequest);
+};
+
+const getRawAssignments = async (access_token, options) => {
+  try {
+    await syncAllCoursesAssignments(access_token, options);
+    const data = await readAssignmentsDb(options);
+    return data;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+exports.getAssignedAssignments = async (req, res) => {
+  const { year, semester, course_no } = req.query;
+  if (!year || !semester)
+    return res
+      .status(400)
+      .json({ message: 'Year or semester is not provided.' });
+  try {
+    const currentTime = Math.floor(Date.now() / 1000);
+    const data = (
+      await getRawAssignments('BlDgVZFg74TelAWAaPzgCrJd7MxAarVGjPTRu1Aq', {
+        year,
+        semester,
+        course_no,
+      })
+    ).filter((item) => currentTime < Number(item.due_time));
+    data.sort((a, b) => {
+      return a.due_time - b.due_time;
+    });
+
+    return res.json(data);
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(400);
+  }
 };
 
 exports.addAssignment = async (req, res) => {
   const item = {
     ...req.body,
   };
-
   const putParams = {
     Item: item,
     TableName: process.env.aws_assignments_table_name,
--- src/routes/assignmentRoutes.js
+++ src/routes/assignmentRoutes.js
@@ -3,7 +3,7 @@ const assignmentController = require('../controller/assignmentsController');
 const router = express.Router();
 
 router.post('/', assignmentController.addAssignment);
-router.get('/', assignmentController.readAssignmentsMcv);
+router.get('/', assignmentController.getAssignedAssignments);
 
 router.get('/', (req, res) => {
   res.send('hello');
