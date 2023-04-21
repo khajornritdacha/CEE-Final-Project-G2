d0c9cd908c0e5823796ae9b784b31b9a837adc3e
vijak khajornritdacha <vijak13019@hotmail.com>
2023-04-21 14:15:54 +0700

    feat(frontend): add heading and navbar for mobile in dashboard page

--- public/index.js
+++ public/index.js
@@ -1,14 +1,14 @@
-import LoginPage from './pages/loginPage.js';
-import DashBoardPage from './pages/dashboardPage.js';
+// import LoginPage from './pages/loginPage.js';
+// import DashBoardPage from './pages/dashboardPage.js';
 
-import { getUserProfile } from './Utility/utility.js';
+// import { getUserProfile } from './Utility/utility.js';
 
-LoginPage();
-const user = await getUserProfile();
+// LoginPage();
+// const user = await getUserProfile();
 
 // TODO: handle if user is already logged in
 // if (user) {
-//     DashBoardPage();
+// DashBoardPage();
 // } else {
 //     LoginPage();
 // }
--- public/pages/dashboardPage.js
+++ public/pages/dashboardPage.js
@@ -1,5 +1,10 @@
 export default function DashBoardPage() {
   const navBar = `
-    
+  <nav class="flex align-center space-around primary fixed bot-0 navbar">
+  <i class="fa-solid fa-house fa-2xl"></i>
+  <i class="fa-solid fa-question fa-2xl"></i>
+  <i class="fa-solid fa-check fa-2xl"></i>
     `;
+
+  document.getElementById('app').innerHTML = navBar;
 }
